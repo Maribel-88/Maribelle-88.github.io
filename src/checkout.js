a9343097ad7a974e5fd961c4612d7ac15b25c195
Maribel1988 <102556943+Maribel1988@users.noreply.github.com>
2024-02-19 15:46:41 +0000

    Added total item on check

--- src/checkout.js
+++ src/checkout.js
@@ -1,7 +1,8 @@
 let checkOutDiv = document.getElementById("checkoutDiv");
 let checkAmount = document.getElementById("checkOutAmount");
+let subCheckOut = document.getElementById("subCheckout");  
 let shoppingCart = document.getElementById("shopping-cart");
-
+    console.log(shoppingCart);
 let basket1 = JSON.parse(localStorage.getItem("data")) || [];
 
 let TotalAmount1 = () => {
@@ -14,7 +15,7 @@ let TotalAmount1 = () => {
     }).reduce((x,y)=>x+y, 0);
     // console.log(amount);
     checkAmount.innerHTML = `
-    <h2>Total Amount : £ ${amount}</h2>
+    <h5>Total Amount: £ ${amount}.00</h5>
     
     `;
    
@@ -31,36 +32,54 @@ let generateItems = () => {
         
         let { id, item } = x;
         let search = shopItemsData.find((y)=>y.id === id) || [];
+       item * search.price;
         let {img, name, price}= search;
         return`
         
-                  
+                
   <table class="table">
-  <thead class="thead-light">
+  
     <tr>
       <th scope="col">Product name</th>
       <th scope="col">Price</th>
-      <th scope="col">Column £</th>
+      <th scope="col">Quantity</th>
+      <th scope="col">Subtotal</th>
     </tr>
-  </thead>
+ 
   <tbody>
     <tr >
-      <th scope="row" id="itemName">${name}</th>
+      <td >${name}</td>
       <td >${price}</td>
+      <td >${item}</td>
       <td>${item * search.price}</td>
+      
+
       
     </tr>
   </tbody>
 </table>
                    
     `
-             
+       
        })
        .join(""));
     }
    };
 
    generateItems();
+
+  function checkoutItems() {
+    let amount1 = basket.map((x) =>{
+      let { item, id} = x;
+      let search = shopItemsData.find((y)=> y.id === id) || [];
+      return item ;
+      }).reduce((x,y)=>x+y, 0);
+
+      subCheckOut.innerHTML = `Total Item(s): (${amount1})`
+    };
+    
+  checkoutItems(); 
+
 
 
 
